493ed8c9f93891eceb4c458018123ede3b81d293
QizhouD <dunicornstate@gmail.com>
2025-11-06 22:41:57 +0800

    feat:Adding a main menu, Random enemy spawning，player UI with health bar(unfinished)

---
+++ SFML/include/entities/Mushroom.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "Entity.h"
+#include "../components/HealthComponent.h"
+#include "../components/VelocityComponent.h"
+#include "../components/ColliderComponent.h"
+#include <memory>
+
+class Mushroom : public Entity {
+public:
+    static constexpr int defaultHealth = 30;
+    static constexpr float moveSpeed = 50.f; // pixels per second
+
+    Mushroom();
+    ~Mushroom() override = default;
+
+    void init(const std::string& textureFile, float scale) override;
+    void update(Game* game, float elapsed) override;
+
+    std::shared_ptr<HealthComponent> getHealthComp() const { return health; }
+    std::shared_ptr<VelocityComponent> getVelocityComp() const { return velocity; }
+
+private:
+    std::shared_ptr<HealthComponent> health;
+    std::shared_ptr<VelocityComponent> velocity;
+    std::shared_ptr<ColliderComponent> collider;
+};
